5f48211d0e106ff1946f764a39a0b630ea63abd9
celsius narhwal <hello@celsiusnarhwal.dev>
2023-07-25 21:49:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Courier
+
+Courier is an internal domain forwarding service for [celsiusnarhwal.dev](https://celsiusnarhwal.dev) and its
+subdomains. It's also responsible for serving a custom 404 page when you try to access a subdomain of `celsiusnarhwal.dev`
+that doesn't exist and for correcting your spelling if you accidentally type `celsiusnarwhal.dev` instead of `celsiusnarhwal.dev`
+into your browser.
+
+## License
+
+Courier is licensed under the [MIT License](LICENSE.md).
---
+++ api/index.py
@@ -0,0 +1,117 @@
+import random
+import string
+
+import aiohttp
+import starlette.datastructures
+import tomlkit as toml
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from path import Path
+from pydantic import BaseModel, TypeAdapter, ValidationError, model_validator
+from typing_extensions import Self
+from yarl import URL
+
+HERE = Path(__file__).parent
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=HERE / "static"), name="static")
+
+templates = Jinja2Templates(directory=HERE / "templates")
+
+
+class Courier(BaseModel):
+    target: str
+    path: bool = False
+    permanent: bool = False
+
+    @classmethod
+    def create(cls, txt_record: str) -> Self:
+        data = {}
+
+        for attr in txt_record.split(";"):
+            try:
+                key, value = attr.split("=")
+            except ValueError:
+                raise HTTPException(status_code=400, detail="Invalid TXT record")
+
+            data[key] = value
+
+        try:
+            return TypeAdapter(Courier).validate_python(data)
+        except ValidationError:
+            raise HTTPException(status_code=400, detail="Invalid TXT record")
+
+    @property
+    def status_code(self):
+        return 308 if self.permanent else 307
+
+    def destination(self, origin: starlette.datastructures.URL):
+        destination = URL(self.target)
+
+        if self.path:
+            destination = destination / origin.path.lstrip("/")
+
+        return destination
+
+
+class NotFoundMessage(BaseModel):
+    text: str
+    punctuation: str = None
+    source: str
+
+    # noinspection PyMethodParameters
+    @model_validator(mode="after")
+    def validate(cls, obj: Self):
+        terminus = obj.text[-1]
+        obj.punctuation = terminus if terminus in string.punctuation else "."
+        obj.text = obj.text.rstrip(string.punctuation)
+        return obj
+
+    @classmethod
+    def get(cls) -> Self:
+        messages = toml.load((HERE / "messages.toml").open())["messages"]
+        return TypeAdapter(NotFoundMessage).validate_python(random.choice(messages))
+
+
+@app.api_route("/{_:path}", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+async def resolver(request: Request):
+    host = request.client.host
+
+    if host == "courier.celsiusnarhwal.dev":
+        return RedirectResponse(
+            url="https://github.com/celsiusnarhwal/courier", status_code=308
+        )
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://dns.google/resolve",
+            params={"name": f"courier.{request.client.host}", "type": "TXT"},
+        ) as resp:
+            resp.raise_for_status()
+            dns = await resp.json()
+
+    if dns.get("Status") != 0:
+        raise HTTPException(404)
+
+    courier_record = next(
+        (record for record in dns.get("Answer", []) if record), {}
+    ).get("data")
+
+    if not courier_record:
+        raise HTTPException(404)
+
+    courier = Courier.create(courier_record)
+
+    return RedirectResponse(
+        url=courier.destination(request.url), status_code=courier.status_code
+    )
+
+
+@app.exception_handler(404)
+async def not_found(request: Request, _):
+    return templates.TemplateResponse(
+        "404.html", {"request": request, "message": NotFoundMessage.get()}
+    )
